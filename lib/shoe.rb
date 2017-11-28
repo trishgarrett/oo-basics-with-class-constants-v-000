565572635f86ca2581653354f82f37173da56817
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-28 15:36:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brands
   end
 
   def cobble
@@ -13,8 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand= (brand)
-    @brand = brand
-    BRANDS << brands
-  end
+  # def brand= (brand)
+  #   @brand = brand
+  #   BRANDS << brands
+  # end
 end
